8ada606fc0591cb3873522b2a30fa6bc88e39df2
IVAN VERA <ivandejesusvera@gmail.com>
2025-02-01 23:52:58 -0500

    Ejercicios 27/01/2025 Y 29/01/2024

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejercicio 1 - Cálculo de área del círculo
+            Console.WriteLine("Calcula el área de un círculo:");
+            Console.WriteLine("Ingrese el radio:");
+            float radio = float.Parse(Console.ReadLine());
+            double area = CalcularAreaCirculo(radio);
+            Console.WriteLine($"El área del círculo es: {area}");
+
+            // Ejercicio 2 - Suma de números
+            int sumaTotal = 0;
+            int numeroIngresado;
+            do
+            {
+                Console.WriteLine("Ingrese un número (o ingrese -1 para salir):");
+
+                if (int.TryParse(Console.ReadLine(), out numeroIngresado))
+                {
+                    if (numeroIngresado == -1)
+                    {
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+                    }
+                    sumaTotal += numeroIngresado;
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, ingrese un número válido.");
+                }
+
+            } while (true);
+
+            Console.WriteLine($"La suma total es: {sumaTotal}");
+        }
+
+        static double CalcularAreaCirculo(float radio)
+        {
+            return Math.PI * Math.Pow(radio, 2);
+        }
+    }
+}
